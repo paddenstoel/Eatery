7b0cea47dab2b28096e3181b5474bc944b618f6c
Victoria Androsenko <androsenko.victoria@gmail.com>
2023-01-21 20:20:56 +0600

    created constructors and methods for adding and deleting a dish to an order

--- src/Order.java
+++ src/Order.java
@@ -1,5 +1,40 @@
 public class Order {
 
+    private final static int DEFAULT_DISHES_LENGTH = 16;
     private Dish[] dishes;
     private int numberOfOrderedDishes;
+
+    public Order() {
+        this.dishes = new Dish[DEFAULT_DISHES_LENGTH];
+    }
+
+    public Order(int arraySize) {
+        this.dishes = new Dish[arraySize];
+    }
+
+    public Order(Dish[] dishes) {
+
+    }
+
+    public boolean addDishToOrder(Dish dish) {
+        for (int i = 0; i < this.dishes.length; i++) {
+            if (this.dishes[i] == null) {
+                this.dishes[i] = dish;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean deleteDishFromOrder(String name) {
+        for (int i = 0; i < this.dishes.length; i++) {
+            if (this.dishes[i].getName().equals(name)) {
+                this.dishes[i] = null;
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
