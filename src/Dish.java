7e289b10b48e9e72d1cc578e2e1ac6d125b7d963
Victoria Androsenko <androsenko.victoria@gmail.com>
2023-01-21 18:34:08 +0600

    added getters and setters for all class fields

--- src/Dish.java
+++ src/Dish.java
@@ -1,4 +1,5 @@
 public class Dish {
+
     private String name;
     private double price;
     private String description;
@@ -12,6 +13,30 @@ public class Dish {
     public Dish(double price, String name, String description) {
         this.price = price;
         this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
